61b31a7efa143b6d59b47880fdc69cb558ca8e7f
LucasAndre23 <146771952+LucasAndre23@users.noreply.github.com>
2023-12-15 17:33:09 -0300

    servidor.py

--- servidor.py
+++ servidor.py
@@ -5,6 +5,66 @@ import threading
 bloqueio_assentos = threading.Lock()
 
 # Esta função é executada em uma nova thread para cada cliente que se conecta ao servidor.
+# Dicionário para mapear os comandos do usuário para os comandos AOT
+PROTOCOLO_AOT = {
+    'assentos_disponiveis': 'ASSD',
+    'reservar_assentos': 'RESV',  
+    'cancelar_reserva': 'CANC'
+}
+
+ERROS_AOT = {
+    444: 'Comando não reconhecido',
+    # criar mais erros...
+}
+
+def trata_msg(request):
+    # Usa o bloqueio para garantir que apenas uma thread acesse a lista de assentos de cada vez
+    bloqueio_assentos.acquire()
+    
+    # Traduz o comando do usuário para o comando AOT correspondente
+    comando_aot = PROTOCOLO_AOT.get(request.split(' ')[0], 'Comando não reconhecido')
+    
+    # Se o comando AOT não for reconhecido, retorna uma mensagem de erro
+    if comando_aot == 'Comando não reconhecido':
+        resposta = 'ERRO 444'
+    else:
+        # Processa o comando do usuário de acordo com o comando AOT correspondente
+        if comando_aot == 'ASSD':
+            # Processa o comando 'assentos_disponiveis'
+            if assentos:
+                resposta = ','.join(assentos)
+                print ('ASSD\n+OK')
+                
+            else :
+                resposta ='Desculpe, todos os assentos foram vendidos/reservados.'
+        elif comando_aot == 'RESV':  # Atualizado para 'RESV'
+            # Processa o comando 'reservar_assentos'
+            partes = request.split(' ')
+            if len(partes) < 2:
+                resposta = "Por favor, forneça o número do assento que deseja reservar."
+            else:
+                assento = partes[1]
+                if assento in assentos:
+                    assentos.remove(assento)
+                    resposta = "Assento " + assento + " reservado com sucesso!"
+                else:
+                    resposta = "Desculpe, o assento " + assento + " não está disponível."
+        elif comando_aot == 'CANC':
+            # Processa o comando 'cancelar_reserva'
+            partes = request.split(' ')
+            if len(partes) < 2:
+                resposta = "Por favor, forneça o número do assento cuja reserva deseja cancelar."
+            else:
+                assento = partes[1]
+                assentos.append(assento)
+                resposta = "Reserva para o assento " + assento + " cancelada com sucesso!"
+    
+    # Libera o bloqueio para que outras threads possam acessar a lista de assentos.
+    bloqueio_assentos.release()
+    
+
+    return resposta
+
 def tratar_cliente(socket_cliente):
     while True:
         # Recebe a solicitação do cliente
@@ -16,52 +76,7 @@ def tratar_cliente(socket_cliente):
             print ('Cliente desconectado.')
             break
         
-        # Usa o bloqueio para garantir que apenas uma thread acesse a lista de assentos de cada vez
-        bloqueio_assentos.acquire()
-        
-        # Se o cliente solicitar os assentos disponíveis, envia a lista de assentos disponíveis.
-        if request == 'assentos_disponiveis':
-            if assentos:
-                resposta = ','.join(assentos)
-            else :
-                resposta ='Desculpe, todos os assentos foram vendidos/reservados.'
-        # Se o cliente solicitar para reservar um assento, verifica se o assento está disponível e, em caso afirmativo, reserva o assento.
-        elif request.startswith('reservar_assentos'):
-            partes = request.split(' ')
-            if len(partes) < 2:
-                resposta = "Por favor, forneça o número do assento que deseja reservar."
-            else:
-                assento = partes[1]
-                if assento in assentos:
-                    assentos.remove(assento)
-                    resposta = "Assento " + assento + " reservado com sucesso!"
-                else:
-                    resposta = "Desculpe, o assento " + assento + " não está disponível."
-        # Se o cliente solicitar para comprar um ingresso, verifica se o assento está disponível e, em caso afirmativo, vende o ingresso.
-        elif request.startswith('comprar_ingressos'):
-            partes = request.split(' ')
-            if len(partes) < 2:
-                resposta = "Por favor, forneça o número do assento que deseja comprar."
-            else:
-                assento = partes[1]
-                if assento not in assentos:
-                    resposta = "Ingresso para o assento " + assento + " comprado com sucesso!"
-                else:
-                    resposta = "Desculpe, o assento " + assento + " ainda está disponível, você precisa reservá-lo primeiro."
-        # Se o cliente solicitar para cancelar uma reserva, cancela a reserva.
-        elif request.startswith('cancelar_reserva'):
-            partes = request.split(' ')
-            if len(partes) < 2:
-                resposta = "Por favor, forneça o número do assento cuja reserva deseja cancelar."
-            else:
-                assento = partes[1]
-                assentos.append(assento)
-                resposta = "Reserva para o assento " + assento + " cancelada com sucesso!"
-        else:
-            resposta = 'Comando não reconhecido.'
-        
-        # Libera o bloqueio para que outras threads possam acessar a lista de assentos.
-        bloqueio_assentos.release()
+        resposta = trata_msg(request)
         
         print(resposta)
         # Envia a resposta ao cliente
@@ -73,7 +88,6 @@ def tratar_cliente(socket_cliente):
     
     # Fecha o socket do cliente quando o cliente envia 'bye'
     socket_cliente.close()
-
 
 # Esta função inicia o servidor.
 def iniciar_servidor():
