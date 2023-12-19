9033d665e5e8c79c63272cecf116f047e927d54a
LucasAndre23 <146771952+LucasAndre23@users.noreply.github.com>
2023-12-18 22:40:40 -0300

    servidor.py

--- servidor.py
+++ servidor.py
@@ -1,43 +1,59 @@
+# Importa os módulos necessários
 import socket
 import threading
+
+# Dicionário para mapear os comandos do usuário para os comandos AOT
+PROTOCOLO_AOT = {
+    'assentos_disponiveis': 'ASSD',
+    'reservar_assentos': 'RESV',
+    'cancelar_reserva': 'CANC'
+}
+
+# Dicionário para mapear os códigos de erro para as mensagens de erro
+ERROS_AOT = {
+    'ERRO 444': 'Comando não reconhecido',
+}
+
+# Lista de assentos disponíveis
+assentos = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
 
 # Cria um bloqueio para a lista de assentos. Isso é usado para prevenir condições de corrida.
 bloqueio_assentos = threading.Lock()
 
-# Esta função é executada em uma nova thread para cada cliente que se conecta ao servidor.
-# Dicionário para mapear os comandos do usuário para os comandos AOT
-PROTOCOLO_AOT = {
-    'assentos_disponiveis': 'ASSD',
-    'reservar_assentos': 'RESV',  
-    'cancelar_reserva': 'CANC'
-}
 
-ERROS_AOT = {
-    444: 'Comando não reconhecido',
-    # criar mais erros...
-}
+# Função para mostrar os assentos disponíveis de uma maneira amigável ao usuário
+def mostrar_assentos_disponiveis():
+    response = "\nAssentos Disponíveis:\n  -------------------------"
 
+    for linha in range(1, 4):
+        assentos_disponiveis = []
+        for coluna in range(1, 4):
+            assento = f"{chr(ord('a') + linha - 1)}{coluna}"
+            status = f"[ {assento} ]" if assento in assentos else "[ --- ]"
+            assentos_disponiveis.append(status)
+        response += "\n  " + ' '.join(assentos_disponiveis)
+
+    response += "\n  -------------------------\n"
+    return response
+
+
+# Função para tratar a mensagem do cliente
 def trata_msg(request):
     # Usa o bloqueio para garantir que apenas uma thread acesse a lista de assentos de cada vez
     bloqueio_assentos.acquire()
-    
+
     # Traduz o comando do usuário para o comando AOT correspondente
-    comando_aot = PROTOCOLO_AOT.get(request.split(' ')[0], 'Comando não reconhecido')
-    
+    comando_aot = PROTOCOLO_AOT.get(request.split(' ')[0], 'ERRO 444')
+
     # Se o comando AOT não for reconhecido, retorna uma mensagem de erro
-    if comando_aot == 'Comando não reconhecido':
+    if comando_aot == 'ERRO 444':
         resposta = 'ERRO 444'
     else:
         # Processa o comando do usuário de acordo com o comando AOT correspondente
         if comando_aot == 'ASSD':
             # Processa o comando 'assentos_disponiveis'
-            if assentos:
-                resposta = ','.join(assentos)
-                print ('ASSD\n+OK')
-                
-            else :
-                resposta ='Desculpe, todos os assentos foram vendidos/reservados.'
-        elif comando_aot == 'RESV':  # Atualizado para 'RESV'
+            resposta = mostrar_assentos_disponiveis()
+        elif comando_aot == 'RESV':
             # Processa o comando 'reservar_assentos'
             partes = request.split(' ')
             if len(partes) < 2:
@@ -58,64 +74,68 @@ def trata_msg(request):
                 assento = partes[1]
                 assentos.append(assento)
                 resposta = "Reserva para o assento " + assento + " cancelada com sucesso!"
-    
+
     # Libera o bloqueio para que outras threads possam acessar a lista de assentos.
     bloqueio_assentos.release()
-    
 
     return resposta
 
-def tratar_cliente(socket_cliente):
+
+# Função para tratar o cliente
+def tratar_cliente(socket_cliente, endereco):
+    print('Cliente conectado:', endereco)
     while True:
         # Recebe a solicitação do cliente
         request = socket_cliente.recv(2048).decode()
-        print(f"Recebido: {request}")
-        
+        print(f"Recebido de {endereco}: {request}")
+
         # Se o cliente enviar 'bye', termina a conexão com o cliente.
         if request == 'bye':
-            print ('Cliente desconectado.')
+            print(f'Cliente {endereco} desconectado.')
             break
-        
+
+        # Trata a mensagem do cliente
         resposta = trata_msg(request)
-        
-        print(resposta)
+
+        print(f"Enviando para {endereco}: {resposta}")
         # Envia a resposta ao cliente
-        socket_cliente.send(bytes(resposta,'UTF-8'))
-        
+        socket_cliente.send(bytes(resposta, 'UTF-8'))
+
         if request == 'bye':
-            print ('Cliente desconectado.')
+            print(f'Cliente {endereco} desconectado.')
             break
-    
+
     # Fecha o socket do cliente quando o cliente envia 'bye'
     socket_cliente.close()
 
-# Esta função inicia o servidor.
+
+# Função para iniciar o servidor
 def iniciar_servidor():
+    # Solicita o endereço IP e a porta do servidor ao usuário
+    LOCALHOST = input("Digite o endereço IP do servidor: ")
+    PORTA = int(input("Digite a porta do servidor: "))
+
     # Cria um socket para o servidor
-    servidor= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     # Vincula o socket a um endereço e porta
     servidor.bind((LOCALHOST, PORTA))
     # Começa a ouvir conexões
     servidor.listen(1)
     print("Servidor iniciado")
-    try :
+
+    try:
         while True:
             # Aceita uma nova conexão
             socket_cliente, endereco = servidor.accept()
-            
-            print ('Cliente :',endereco)
+
             # Cria uma nova thread para tratar a conexão
-            thread_cliente = threading.Thread(target=tratar_cliente, args=(socket_cliente,))
+            thread_cliente = threading.Thread(target=tratar_cliente, args=(socket_cliente, endereco))
             thread_cliente.start()
     finally:
         # Fecha o socket do servidor quando o servidor é encerrado.
         servidor.close()
 
-LOCALHOST = "192.168.56.1"
-PORTA = 4000
-# Lista de assentos disponíveis
-assentos = [ 'a1','a2','a3'] #,'b1','b2','b3','c1','c2','c3']
 
 # Inicia o servidor
 iniciar_servidor()
